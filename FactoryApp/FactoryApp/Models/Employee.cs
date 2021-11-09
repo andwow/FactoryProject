0b7a0b0155bacb92316d9233360f7a243d04776d
andwow <53525076+andwow@users.noreply.github.com>
2021-11-09 13:25:52 +0200

    Login

---
+++ FactoryApp/FactoryApp/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryApp.Models
+{
+    class Employee
+    {
+        string username;
+        string password;
+        string lastName;
+        string firstName;
+        string cnp;
+        string gender;
+        DateTime birthday;
+    }
+}
