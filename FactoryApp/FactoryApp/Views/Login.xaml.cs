a2d16619d93eada708a50aeaaa5924fe89ceb8ad
tireacosmin <cosmin.tirea@gmail.com>
2021-11-15 21:11:06 +0200

    Adding Login Menu

--- FactoryApp/FactoryApp/Views/Login.xaml.cs
+++ FactoryApp/FactoryApp/Views/Login.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace FactoryApp.Views
 {
@@ -23,6 +25,38 @@ namespace FactoryApp.Views
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Connection String   
+            SqlConnection con = new SqlConnection("Data Source=COSMIN-ACER;Initial Catalog=Factory;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from Userlogins where UserName=@UserName and Password =@Password", con);
+            cmd.Parameters.AddWithValue("@UserName", username.Text);
+            cmd.Parameters.AddWithValue("@Password", password.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            //Connection open here   
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Successfully loged in");
+                //after successful it will redirect  to next page .  
+                //WelcomePage settingsForm = new WelcomePage();
+                //settingsForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please enter Correct Username and Password");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
